fbb61ad62c6f8f9e811bdf2e87b6537f0ea10372
unknown <mohit16gaikwad@gmail.com>
2022-04-18 12:43:26 +0530

    UC1: Added code for valid Last name

--- src/main/java/lamda_Exp_Day23/Lambda_UserReg.java
+++ src/main/java/lamda_Exp_Day23/Lambda_UserReg.java
@@ -3,23 +3,36 @@ package lamda_Exp_Day23;
 import java.util.regex.Pattern;
 
 @FunctionalInterface
-	interface Check {
-	    boolean check(String a, String b, String c) throws Invalid_User;
-	}
-	public class Lambda_UserReg {
-		
-		public static void main(String[] args) throws Invalid_User {
-			
-	        Check firstName = (String pattern, String fname, String mood) -> {try {
-	            if (mood.equals("Happy")) {
-	                if (Pattern.matches(pattern, fname))
-	                    return true;
-	                else
-	                    return false;
-	            } else
-	                return false;
-	        }catch (NullPointerException e) {
-	            throw new Invalid_User("Please enter proper message");
-	        }};}}
-		          
+interface Check {
+    boolean check(String a, String b, String c) throws Exception;
+}
+public class Lambda_UserReg {
+	public static void main(String[] args) throws Exception {
+        Check firstName = (String pattern, String fname, String mood) -> {try {
+            if (mood.equals("Happy")) {
+                if (Pattern.matches(pattern, fname))
+                    return true;
+                else
+                    return false;
+            } else
+                return false;
+        }catch (NullPointerException e) {
+            throw new Exception("Please enter proper message");
+        }};
+        Check lastName = (String pattern, String lname, String mood) -> {try {
+            if (mood.equals("Happy")) {
+                if (Pattern.matches(pattern, lname))
+                    return true;
+                else
+                    return false;
+            } else
+                return false;
+        }catch (NullPointerException e) {
+            throw new Exception("Please enter proper message");
+        }};    
+	
+	}}
+	          
+
+
 
--- src/main/java/lamda_Exp_Day23/User_Reg.java
+++ src/main/java/lamda_Exp_Day23/User_Reg.java
@@ -3,7 +3,7 @@ package lamda_Exp_Day23;
 import java.util.regex.Pattern;
 
 public class User_Reg {
-	public boolean firstName(String pattern,String firstName,String mood) throws Invalid_User {
+	public boolean firstName(String pattern,String firstName,String mood) throws Exception {
         try {
             if (mood.equals("HAPPY")) {
                 if (Pattern.matches(pattern, firstName))
@@ -16,5 +16,19 @@ public class User_Reg {
             throw new Invalid_User("Please enter proper message");
         }
 }
+	 public boolean lastName(String pattern,String lastName,String mood) throws Exception {
+	        try {
+	            if (mood.equals("HAPPY")) {
+	                if (Pattern.matches(pattern, lastName))
+	                    return true;
+	                else
+	                    return false;
+	            } else
+	                return false;
+	        }catch (NullPointerException e) {
+	            throw new Invalid_User("Please enter proper message");
+	        }
+	    }
 }
+
 
